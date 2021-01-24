0be7cf01858815697a179035c8235711717c6b95
volatill <510112342@qq.com>
2021-01-24 10:10:42 -0800

    U

---
+++ get_graph.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+file=$1
+dataset_array=("webstanford" "dblp") #"pokec" "lj" "orkut" "twitter")
+mkdir ppr-answer
+for dataset in "${dataset_array[@]}"; do
+	mkdir ppr-answer/${dataset}
+	n=$(cat ../../${file}/data/${dataset}/attribute.txt | head -n 1 | cut -d"=" -f 2)
+	cp ../../${file}/data/${dataset}/graph.txt dataset/${dataset}.txt
+	sed -i "1i${n}" dataset/${dataset}.txt
+ 	./TopPPR -d ${dataset} -algo GEN_QUERY -n 100
+	./TopPPR -d ${dataset} -algo GEN_GROUND_TRUTH	-n 100
+done
---
+++ run_topk.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+file=$1
+dataset_array=("dblp") #"pokec" "lj" "orkut" "twitter")
+workload_array=("20" "50" "80")
+mkdir topppr_result
+for dataset in "${dataset_array[@]}"; do
+	for i in "${workload_array[@]}"; do
+		./TopPPR -d ${dataset} -algo TopPPR -r 0.99 -n ${i} -k 500 | tee topppr_result/${dataset}_${i}_result.txt
+	done
+done
+
+for dataset in "${dataset_array[@]}"; do
+	for i in "${workload_array[@]}"; do
+		echo ""
+		echo "dataset:$dataset   query num:$i  "
+		cat topppr_result/${dataset}_${i}_result.txt| tail -n 5
+	done
+done
